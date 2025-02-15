b340140190dc54eebcdb7246c08494e1432739d4
Dilly Fu <login.burgess.fu@gmail.com>
2025-02-16 01:01:32 +0800

    20250216

---
+++ list.py
@@ -0,0 +1,11 @@
+# list 清單 除int, float, bool, str,外 另有list
+
+a = ['benz', 'audi']
+print(a)
+print(a[0])
+print(a[1])
+a.append('bmw')
+print(a)
+print(len(a))
+print('benz' in a)  # 是非題 True, False
+print('kia' in a)
