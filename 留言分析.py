85e05662df370f96540ae7fe867d8036c322d4ed
Dilly Fu <login.burgess.fu@gmail.com>
2025-02-16 22:06:26 +0800

    20250216-5

--- 留言分析.py
+++ 留言分析.py
@@ -4,7 +4,7 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1 # count = count + 1
-		if count % 10000 == 0: # %代表求餘數
+		if count % 50000 == 0: # %代表求餘數
 			print(len(data))
 print('檔案計有:', len(data), '筆留言')
 print(data[0])
@@ -23,3 +23,10 @@ for d in data:
 print('一共有', len(new), '筆留言字母數小於100')
 print(new[0])
 print(new[1])
+
+amazing = []
+for d in data:
+	if 'amazing' in d:
+		amazing.append(d)
+print('一共有', len(amazing), '筆留言提到amazing')
+print(amazing[0])
