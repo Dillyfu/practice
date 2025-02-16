8a9095c65ecf6fa6e0287dda069a02e27adfd724
Dilly Fu <login.burgess.fu@gmail.com>
2025-02-16 18:29:49 +0800

    20250216

--- 留言分析.py
+++ 留言分析.py
@@ -1,5 +1,12 @@
 data = []
-with open('review.txt', 'r') as f:
+count = 0
+with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-print(data)
+		count += 1 # count = count + 1
+		if count % 10000 == 0: # %代表求餘數
+			print(len(data))
+print(len(data))
+print(data[0])
+print('----------------')
+print(data[10])
