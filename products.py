fe7f11ad944ac53d5e7e0b6e1a5def708668de28
Dilly Fu <login.burgess.fu@gmail.com>
2025-02-20 00:11:15 +0800

    20250220

---
+++ products.py
@@ -0,0 +1,17 @@
+products = []
+while True:
+	name = input('請輸入商品名稱:')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格:')
+	products.append([name, price])
+	#上一行是減縮寫法，原始寫法如下
+	# p =[]
+	# p.append(name)
+	# p.append(price)
+	# prosucts.append(p)
+
+print(products)
+
+for p in products:
+	print(p[0], '的價格是:', p[1])
